58a653fbd897d06f1948dca0e918ee39f79de3a6
AniketMasal <104128642+AniketMasal@users.noreply.github.com>
2022-04-28 15:08:48 +0530

    Create app.component.ts

---
+++ Angular10/Day02/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, Output } from '@angular/core';
+import { Products } from './Models/Products';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+
+}
