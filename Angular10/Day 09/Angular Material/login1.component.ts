8813e8ea8b2b03f57afd973dda77f98dd40abd5f
AniketMasal <104128642+AniketMasal@users.noreply.github.com>
2022-05-12 14:10:24 +0530

    Login component html and ts file

---
+++ Angular10/Day 09/Angular Material/login1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login1',
+  templateUrl: './login1.component.html',
+  styleUrls: ['./login1.component.css']
+})
+export class Login1Component implements OnInit {
+
+
+  login_click()
+  {
+    alert("Button click")
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
